4e77f92abf20eab0011e5fe96f7dd609cefdf6b0
Duc Huy Tran <duchuytran943@gmail.com>
2019-09-26 17:09:43 +0700

    Create data.json and student menu app
    Signed-off-by: Duc Huy Tran <duchuytran943@gmail.com>

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function showMenu() {
+    console.log('1. Show all students');
+    console.log('2. Create a new student');
+    console.log('3. Save and Exit');
+}
+
+function main() {
+    showMenu();
+
+}
+
+main();
