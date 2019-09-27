d864b28fe83df870923118befa488510ee451a33
Duc Huy Tran <duchuytran943@gmail.com>
2019-09-27 07:19:00 +0700

    Function showStudents
    show all students have in data.json
    
    Signed-off-by: Duc Huy Tran <duchuytran943@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var readlineSync =  require('readline-sync');
+var fs = require('fs');
+
+var students = [];
+function loadData() {
+    var fileContent = fs.readFileSync('./data.json', { encoding: 'utf8' }); //fileContent is a string
+    // console.log(fileContent);
+    students = JSON.parse(fileContent); //covert string to object students.
+}
 
 function showMenu() {
     console.log('1. Show all students');
@@ -5,8 +14,32 @@ function showMenu() {
     console.log('3. Save and Exit');
 }
 
+function showStudents() {
+    for(var student of students){
+        console.log(student.name, student.age);
+    }
+}
+
 function main() {
+    loadData();
+    // console.log(students);
     showMenu();
+    var option = readlineSync.question('>');
+    switch (option) {
+        case '1':
+            showStudents();
+            break;
+        case '2':
+            showCreateStudent();
+            break;
+        case '3':
+            saveAndExit();
+            break;
+    
+        default:
+            console.log('Wrong option');
+            break;
+    }
 
 }
 
